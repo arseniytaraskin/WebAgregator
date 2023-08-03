c7879e58791292372f4f3145cf161bd66f3b1246
Arseniy <taraskin.arseniy@gmail.com>
2023-08-03 13:01:53 +0500

    добавлена обновленная модель Project

---
+++ my_site/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from users.forms import User
+from django.core.validators import FileExtensionValidator
+
+class ProjectModel(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='projects_images/%Y-%m-%d/')
+    file = models.FileField(validators=[FileExtensionValidator(['zip'])])
+    views_count = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
